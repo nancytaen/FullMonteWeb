bb16ce991456ca9b1e8305afa205607c0aad8948
Charlie Chai <charlie.chai@mail.utoronto.ca>
2019-10-29 19:39:43 -0400

    Added 3 new views: tutorial, simulation and visualization Linked each view to all pages

--- fullMonteWeb/application/urls.py
+++ fullMonteWeb/application/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /application
     path('', views.home, name='home'),
+            
+    # /application/tutorial
+    path('tutorial', views.fmTutorial, name='tutorial'),
+               
+    # /application/simulator
+    path('simulator', views.fmSimulator, name='simulator'),
+               
+    # /application/visualization
+    path('visualization', views.fmVisualization, name='visualization'),
 ]
--- fullMonteWeb/application/views.py
+++ fullMonteWeb/application/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to Full Monte")
-
 # Create your views here.
+
+# homepage
+def home(request):
+    return render(request, "home.html")
+
+# FullMonte Tutorial page
+def fmTutorial(request):
+    return render(request, "tutorial.html")
+
+# FullMonte Simulator page
+def fmSimulator(request):
+    return render(request, "simulator.html")
+
+# FullMonte Output page
+def fmVisualization(request):
+    return render(request, "visualization.html")
