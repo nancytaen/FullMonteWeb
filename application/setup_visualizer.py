43f96016d3803ab213f4254449306a03a262b4e3
galenbrook <galenbrook@gmail.com>
2020-03-08 21:34:54 -0400

    visualization

--- application/setup_visualizer.py
+++ application/setup_visualizer.py
@@ -38,10 +38,9 @@ def visualizer():
                     errdata += chan.recv_stderr(1000)
                 if chan.exit_status_ready():  # If completed
                     break
-                print(outdata)
-                print(errdata)
-                outdata, errdata = b'',b''
 
+            print(outdata)
+            print(errdata)
             retcode = chan.recv_exit_status()
             ssh_transp.close()
 
