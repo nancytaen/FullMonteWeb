9b83f5e18b87438712d3c1fe95327d07e04432d4
Charlie Chai <charlie.chai@mail.utoronto.ca>
2020-03-21 22:05:59 -0400

    added account confirmation

--- application/views.py
+++ application/views.py
@@ -31,9 +31,21 @@ from django.core.files.storage import default_storage
 from django.core import serializers
 import time
 
+from .tokens import account_activation_token
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib import messages
+from django.core.mail import EmailMessage
+
+from decouple import config
+
+send_mail('Subject here', 'Here is the message.', settings.EMAIL_HOST_USER,
+         ['to@example.com'], fail_silently=False)
 
 # Create your views here.
 
@@ -481,12 +493,45 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            #username = form.cleaned_data.get('username')
+            #raw_password = form.cleaned_data.get('password1')
+            #user = authenticate(username=username, password=raw_password)
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your FullMonte account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return render(request, "account_registration.html")
+
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return render(request, "activation_complete.html")
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+def account(request):
+    return render(request, "account.html")
