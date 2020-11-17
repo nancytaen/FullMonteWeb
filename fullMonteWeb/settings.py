d70dd64823cacd1ba14a4e035ce820388eddfe01
helen-23 <helendai23@gmail.com>
2020-11-17 07:21:31 -0500

    removed potgresql for EB deployment

--- fullMonteWeb/settings.py
+++ fullMonteWeb/settings.py
@@ -93,15 +93,11 @@ WSGI_APPLICATION = 'fullMonteWeb.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', 'postgres'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASS', 'sql'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+            'default': {
+                        'ENGINE': 'django.db.backends.sqlite3',
+                                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+                                    }
+            }
 
 
 # Password validation
