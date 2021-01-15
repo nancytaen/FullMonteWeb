433db9bb8a94e57d7992e422f70e8af97dceb5a0
helen-23 <helendai23@gmail.com>
2021-01-15 01:38:54 -0500

    reoprganized and added comments in urls.py for clarity

--- application/urls.py
+++ application/urls.py
@@ -9,12 +9,22 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    ####################### home #######################
     # /application
     path('', views.home, name='home'),
 
+    ####################### tutorial #######################
     # /application/tutorial
     path('tutorial', views.fmTutorial, name='tutorial'),
 
+    ####################### AWS #######################
+    # /application/aws
+    path('aws', views.aws, name='aws'),
+
+    # /application/AWSsetup
+    path('AWSsetup', views.AWSsetup, name='AWSsetup'),
+
+    ####################### simulator #######################
     # /application/simulator
     path('simulator', views.fmSimulator, name='simulator'),
 
@@ -24,30 +34,38 @@ urlpatterns = [
     # /application/simulator_source
     path('simulator_source', views.fmSimulatorSource, name='simulator_source'),
 
+    # /application/running
+    path('running', views.running, name='running'),
+
+    # /application/simulation_fail
+    path('simulation_fail', views.simulation_fail, name='simulation_fail'),
+
+    # /application/simulation_finish
+    path('simulation_finish', views.simulation_finish, name='simulation_finish'),
+
+    ####################### visualization #######################
     # /application/visualization
     path('visualization', views.fmVisualization, name='visualization'),
+
+    # /application/mesh_upload
+    path('mesh_upload', views.visualization_mesh_upload, name='mesh_upload'),
+
+    # /application/runningDVH
+    path('runningDVH', views.runningDVH, name='runningDVH'),
     
+    # /application/displayVisualization
+    path('displayVisualization', views.displayVisualization, name='displayVisualization'),
+    
+    ####################### history #######################
+    # /application/simulation_history
     path('simulation_history', views.simulation_history, name='simulation_history'),
     url(r'download/(?P<filename>[-\w_\\-\\.]+)$', views.fileDownloadView.as_view(), name='file_download'),
 
-    # /application/about
-    path('about', views.about, name='about'),
-
-    # /application/download_output
-    path('download_output', views.downloadOutput, name='download_output'),
-
-    # /application/kernel_info
-    path('kernel_info', views.kernelInfo, name='kernel_info'),
-
-    # /application/download_preset
-    path('download_preset', views.downloadPreset, name='download_preset'),
-
+    ####################### presets #######################
     # /application/create_preset_material
     path('create_preset_material', views.createPresetMaterial, name='create_preset_material'),
 
-    # /application/aws
-    path('aws', views.aws, name='aws'),
-
+    ####################### account & registration #######################
     url(r'^password_reset/$', auth_views.PasswordResetView.as_view() , name='password_reset'),
     url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
@@ -71,21 +89,22 @@ urlpatterns = [
 
     path('please_login', views.please_login, name='please_login'),
 
-    path('running', views.running, name='running'),
-    
-    path('AWSsetup', views.AWSsetup, name='AWSsetup'),
+    ####################### not in use (for now) #######################
+    # /application/about
+    path('about', views.about, name='about'),
 
-    path('simulation_fail', views.simulation_fail, name='simulation_fail'),
+    # /application/download_output
+    path('download_output', views.downloadOutput, name='download_output'),
 
-    path('simulation_finish', views.simulation_finish, name='simulation_finish'),
-               
+    # /application/kernel_info
+    path('kernel_info', views.kernelInfo, name='kernel_info'),
+
+    # /application/download_preset
+    path('download_preset', views.downloadPreset, name='download_preset'),
+
+    # /application/heroku_timeout    
     path('heroku_timeout', views.heroku_timeout, name='heroku_timeout'),
 
-    path('mesh_upload', views.visualization_mesh_upload, name='mesh_upload'),
-
-    path('runningDVH', views.runningDVH, name='runningDVH'),
-    
-    path('displayVisualization', views.displayVisualization, name='displayVisualization'),
 ]
 
 if settings.DEBUG:
