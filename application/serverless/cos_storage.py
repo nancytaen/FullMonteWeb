9e52647b66d9e54bfe123891a5344e4114ae0170
Nancy Li <nancy.taen.li@gmail.com>
2023-04-01 01:41:30 -0400

    add unique id to serverless tcl files and refactored

--- application/serverless/cos_storage.py
+++ application/serverless/cos_storage.py
@@ -1,24 +1,56 @@
-import shortuuid
-
 from django.conf import settings
 import ibm_boto3
+from ibm_botocore.client import Config
 
 
-def generate_unique_request_id():
-    return shortuuid.uuid()
+def handle_uploaded_file(request_f, dest_f):
+    with open(dest_f, 'wb+') as destination:
+        for chunk in request_f.chunks():
+            destination.write(chunk)
 
 
-def insert_id_to_filename(filename, request_id):
-    # split filename into basename and extension
-    # basename-<request_id>.ext
-    partitions = filename.rsplit('.', 1)
-    return f'{partitions[0]}-{request_id}.{partitions[1]}'
+def cos_search_prefix(bucket_name, prefix):
+    client = ibm_boto3.client(
+        "s3",
+        ibm_api_key_id=settings.IBM_COS_API_KEY_ID,
+        ibm_service_instance_id=settings.IBM_COS_SERVICE_INSTANCE_CRN,
+        ibm_auth_endpoint=settings.IBM_COS_AUTH_ENDPOINT,
+        config=Config(signature_version="oauth"),
+        endpoint_url=settings.IBM_COS_ENDPOINT_URL
+    )
+    response = client.list_objects_v2(
+        Bucket=bucket_name,
+        EncodingType='url',
+        MaxKeys=123,
+        Prefix=prefix,
+        FetchOwner=True | False,
+    )
+
+    return [filename['Key'] for filename in response.get('Contents', [])]
 
 
-def extract_id_from_filename(filename):
-    # use - and . as delimiters to extract the request id from filename
-    base = filename.rsplit('.', 1)[0]
-    return base.rsplit('-', 1)[1]
+def cos_upload_file(bucket_name, item_name, file_path):
+    print("Starting large file upload for {0} to bucket: {1}".format(item_name, bucket_name))
+
+    # Create client connection
+    cos_cli = ibm_boto3.client(
+        "s3",
+        ibm_api_key_id=settings.IBM_COS_API_KEY_ID,
+        ibm_service_instance_id=settings.IBM_COS_SERVICE_INSTANCE_CRN,
+        ibm_auth_endpoint=settings.IBM_COS_AUTH_ENDPOINT,
+        config=Config(signature_version="oauth"),
+        endpoint_url=settings.IBM_COS_ENDPOINT_URL
+    )
+
+    try:
+        # initiate file upload
+        cos_cli.upload_file(file_path, bucket_name, item_name)
+
+        print("File upload complete!")
+    except Exception as e:
+        print("Unable to complete large file upload: {0}".format(e))
+    finally:
+        print("completed file transfer")
 
 
 def cos_presigned_url(bucket_name, key_name, http_method='get_object', expiration=600):
@@ -33,6 +65,6 @@ def cos_presigned_url(bucket_name, key_name, http_method='get_object', expiratio
                            endpoint_url=settings.IBM_COS_ENDPOINT_URL
                            )
     signed_url = cos.generate_presigned_url(http_method, Params={
-                                           'Bucket': bucket_name, 'Key': key_name}, ExpiresIn=expiration)
+        'Bucket': bucket_name, 'Key': key_name}, ExpiresIn=expiration)
     print("presigned download URL =>" + signed_url)
     return signed_url
---
+++ application/serverless/parameters.py
@@ -0,0 +1,27 @@
+import shortuuid
+
+
+class ServerlessParameters:
+    SERVERLESS_REQUEST = 'serverless_request'
+    ID = 'request_id'
+    MESH = 'mesh_name'
+    TCL = 'tcl_name'
+    BASE = 'base_name'
+
+    def __init__(self, mesh_name):
+        self.request_id = shortuuid.uuid()
+        self.mesh_name = self.insert_id_to_filename(mesh_name)
+        self.base_name = self.mesh_name[:-3]  # base name includes . e.g. something.vtk -> something.
+        self.tcl_name = f'{self.base_name}tcl'
+
+    def insert_id_to_filename(self, filename):
+        # split filename into basename and extension
+        # basename-<request_id>.ext
+        partitions = filename.rsplit('.', 1)
+        return f'{partitions[0]}-{self.request_id}.{partitions[1]}'
+
+    @staticmethod
+    def extract_id_from_filename(filename):
+        # use - and . as delimiters to extract the request id from filename
+        base = filename.rsplit('.', 1)[0]
+        return base.rsplit('-', 1)[1]
